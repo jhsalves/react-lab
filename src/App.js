4e70791930b54531e608eb16902344ef611c147f
Jonathas Alves de Oliveira <cast.jonathas.utic@sebrae.com.br>
2021-04-27 23:52:36 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './components/Layout/Header';
+import { Fragment } from 'react';
+
+function App() {
+  return (
+    <Fragment>
+      <Header/>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,12 @@
+import classes from './HeaderCartButton.module.css'
+const HeaderCartButton = props => {
+    return (
+        <button>
+            <span>
+                
+            </span>
+        </button>
+    )
+}
+
+export default HeaderCartButton;
